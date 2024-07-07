4da25a805fd93efdc6a92c5473906fdd1f1ed086
Ashish Jain <ashishjain99@yahoo.com>
2024-07-07 17:04:35 -0600

    Added sql folder with some sample inserts

---
+++ sql/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO topic
+(topic_name)
+VALUES
+    ('Python 101')
+,   ('Python Flask Framework')
+,   ('Rust 101')
+,   ('JavaScript 101')
+,   ('JavaScript Node Framework');
+
+INSERT INTO tag
+(tag_name)
+VALUES
+    ('Python')
+,   ('Flask')
+,   ('Rust')
+,   ('JavaScript')
+,   ('Node Js');
