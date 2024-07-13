e918dbed50ff7446e927c4c871c883557a47e0c6
Ashish Jain <ashishjain99@yahoo.com>
2024-07-13 15:41:08 -0600

    Fixed Navbar

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+let hamburger = document.getElementById('ham-button');
+let navbar = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', function()
+{
+    if(navbar.style.display == 'none')
+    {
+        navbar.style.display = 'block';
+    }
+    else
+    {
+        navbar.style.display = 'none';
+    }
+});
