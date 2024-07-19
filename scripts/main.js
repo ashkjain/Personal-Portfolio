f9c1e8657709529a9d18575146a604aa0387b627
Ashish Jain <ashishjain99@yahoo.com>
2024-07-18 20:24:04 -0600

    Till my skills completed

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,28 @@ hamburger.addEventListener('click', function()
     }
 });
 
-if (window.innerWidth >  768)
-{
-    navbar.innerHTML += "<li class='logo-name links'>AJ</li>"
+// Function to handle screen resize
+function handleResize() {
+    var logoElement = document.querySelector('.logo-name');
+
+    if (window.innerWidth > 769) {
+        // Add logo element if it doesn't exist
+        if (!logoElement) {
+            navbar.innerHTML += "<li class='logo-name links'>AJ</li>";
+        }
+    } else {
+        // Remove logo element if screen size is 769px or less
+        if (logoElement) {
+            logoElement.remove();
+        }
+    }
 }
+
+// Initial check on page load
+handleResize();
+
+// Event listener for window resize
+window.addEventListener('resize', function() {
+    // Call handleResize function on resize
+    handleResize();
+});
